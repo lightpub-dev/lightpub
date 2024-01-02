dea7c867385ea26e7512d9ebf60231e016de92c0
das08 <41512077+das08@users.noreply.github.com>
2024-01-02 14:48:53 +0900

    init frontend

---
+++ frontend/src/views/MainView.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="main-view">
+        <h1>Main View</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "MainView"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontend/vite.config.ts
@@ -0,0 +1,14 @@
+import {fileURLToPath} from "url";
+import {defineConfig} from 'vite'
+import vue from '@vitejs/plugin-vue'
+import WindiCSS from 'vite-plugin-windicss'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
+        }
+    },
+    plugins: [vue(), WindiCSS()]
+})
