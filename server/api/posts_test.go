c16d92d2867dd6d3de5a6d4c40afbf0da9683580
tinaxd <31384595+tinaxd@users.noreply.github.com>
2024-01-04 14:48:47 +0900

    testReaction

--- server/api/posts_test.go
+++ server/api/posts_test.go
@@ -79,3 +79,48 @@ func TestReplyPost(t *testing.T) {
 		assert.Equal(t, 201, failedReplyPost.Rec.Code)
 	})
 }
+
+func TestReaction(t *testing.T) {
+	dbInit(t, testutils.DefaultDBConnection())
+
+	login := createAdminUser(t)
+	login2 := createAdminUser2(t)
+
+	req := setupRequestAuth(t, "POST", "/post", login.Token, map[string]interface{}{
+		"content": "Hello @ lightpub",
+		"privacy": "public",
+	})
+
+	assert.Equal(t, 201, req.Rec.Code)
+	var publicPost postResponse
+	testutils.SchemaCheck(t, &publicPost, req.Rec.Body)
+
+	req = setupRequestAuth(t, "POST", "/post", login.Token, map[string]interface{}{
+		"content": "very private info",
+		"privacy": "private",
+	})
+
+	assert.Equal(t, 201, req.Rec.Code)
+	var privatePost postResponse
+	testutils.SchemaCheck(t, &privatePost, req.Rec.Body)
+
+	t.Run("Add a reaction to a public post", func(t *testing.T) {
+		addReaction := setupRequestAuth(t, "PUT", "/post/"+publicPost.ID+"/reaction/+1", login2.Token, nil)
+		assert.Equal(t, 200, addReaction.Rec.Code)
+	})
+
+	t.Run("Delete a reaction from a public post", func(t *testing.T) {
+		addReaction := setupRequestAuth(t, "DELETE", "/post/"+publicPost.ID+"/reaction/+1", login2.Token, nil)
+		assert.Equal(t, 200, addReaction.Rec.Code)
+	})
+
+	t.Run("Add a reaction to a private post", func(t *testing.T) {
+		addReaction := setupRequestAuth(t, "PUT", "/post/"+privatePost.ID+"/reaction/+1", login2.Token, nil)
+		assert.Equal(t, 404, addReaction.Rec.Code)
+	})
+
+	t.Run("Delete a reaction from a private post", func(t *testing.T) {
+		addReaction := setupRequestAuth(t, "DELETE", "/post/"+privatePost.ID+"/reaction/+1", login2.Token, nil)
+		assert.Equal(t, 404, addReaction.Rec.Code)
+	})
+}
