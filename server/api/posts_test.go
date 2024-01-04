36359fd4a49467b933ad4ba09834ed051b789f59
tinaxd <31384595+tinaxd@users.noreply.github.com>
2024-01-04 14:55:48 +0900

    favorite

--- server/api/posts_test.go
+++ server/api/posts_test.go
@@ -124,3 +124,93 @@ func TestReaction(t *testing.T) {
 		assert.Equal(t, 404, addReaction.Rec.Code)
 	})
 }
+
+func TestFavorite(t *testing.T) {
+	dbInit(t, testutils.DefaultDBConnection())
+
+	login := createAdminUser(t)
+	login2 := createAdminUser2(t)
+
+	req := setupRequestAuth(t, "POST", "/post", login.Token, map[string]interface{}{
+		"content": "Hello @ lightpub",
+		"privacy": "public",
+	})
+
+	assert.Equal(t, 201, req.Rec.Code)
+	var publicPost postResponse
+	testutils.SchemaCheck(t, &publicPost, req.Rec.Body)
+
+	req = setupRequestAuth(t, "POST", "/post", login.Token, map[string]interface{}{
+		"content": "very private info",
+		"privacy": "private",
+	})
+
+	assert.Equal(t, 201, req.Rec.Code)
+	var privatePost postResponse
+	testutils.SchemaCheck(t, &privatePost, req.Rec.Body)
+
+	t.Run("Add to favorite to a public post", func(t *testing.T) {
+		addReaction := setupRequestAuth(t, "PUT", "/post/"+publicPost.ID+"/favorite", login2.Token, nil)
+		assert.Equal(t, 200, addReaction.Rec.Code)
+	})
+
+	t.Run("Delete to favorite from a public post", func(t *testing.T) {
+		addReaction := setupRequestAuth(t, "DELETE", "/post/"+publicPost.ID+"/favorite", login2.Token, nil)
+		assert.Equal(t, 200, addReaction.Rec.Code)
+	})
+
+	t.Run("Add to favorite to a private post", func(t *testing.T) {
+		addReaction := setupRequestAuth(t, "PUT", "/post/"+privatePost.ID+"/favorite", login2.Token, nil)
+		assert.Equal(t, 404, addReaction.Rec.Code)
+	})
+
+	t.Run("Delete to favorite from a private post", func(t *testing.T) {
+		addReaction := setupRequestAuth(t, "DELETE", "/post/"+privatePost.ID+"/favorite", login2.Token, nil)
+		assert.Equal(t, 404, addReaction.Rec.Code)
+	})
+}
+
+func TestBookmark(t *testing.T) {
+	dbInit(t, testutils.DefaultDBConnection())
+
+	login := createAdminUser(t)
+	login2 := createAdminUser2(t)
+
+	req := setupRequestAuth(t, "POST", "/post", login.Token, map[string]interface{}{
+		"content": "Hello @ lightpub",
+		"privacy": "public",
+	})
+
+	assert.Equal(t, 201, req.Rec.Code)
+	var publicPost postResponse
+	testutils.SchemaCheck(t, &publicPost, req.Rec.Body)
+
+	req = setupRequestAuth(t, "POST", "/post", login.Token, map[string]interface{}{
+		"content": "very private info",
+		"privacy": "private",
+	})
+
+	assert.Equal(t, 201, req.Rec.Code)
+	var privatePost postResponse
+	testutils.SchemaCheck(t, &privatePost, req.Rec.Body)
+
+	t.Run("Add to bookmark to a public post", func(t *testing.T) {
+		addReaction := setupRequestAuth(t, "PUT", "/post/"+publicPost.ID+"/bookmark", login2.Token, nil)
+		assert.Equal(t, 200, addReaction.Rec.Code)
+	})
+
+	t.Run("Delete to bookmark from a public post", func(t *testing.T) {
+		addReaction := setupRequestAuth(t, "DELETE", "/post/"+publicPost.ID+"/bookmark", login2.Token, nil)
+		assert.Equal(t, 200, addReaction.Rec.Code)
+	})
+
+	t.Run("Add to bookmark to a private post", func(t *testing.T) {
+		addReaction := setupRequestAuth(t, "PUT", "/post/"+privatePost.ID+"/bookmark", login2.Token, nil)
+		assert.Equal(t, 404, addReaction.Rec.Code)
+	})
+
+	t.Run("Delete to bookmark from a private post", func(t *testing.T) {
+		addReaction := setupRequestAuth(t, "DELETE", "/post/"+privatePost.ID+"/bookmark", login2.Token, nil)
+		assert.Equal(t, 404, addReaction.Rec.Code)
+	})
+}
