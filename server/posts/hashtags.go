2b5595213c84c89835fe8bfaa127f8f2e64f6a2a
tinaxd <31384595+tinaxd@users.noreply.github.com>
2024-01-10 16:59:17 +0900

    Add hashtag functionality to FindHashtags function

--- server/posts/hashtags.go
+++ server/posts/hashtags.go
@@ -1,5 +1,49 @@
 package posts
 
+import "unicode"
+
 func FindHashtags(content string) []string {
-	return []string{}
+	hashtags := []string{}
+	inHashtag := -1
+	ignoreCurrentWord := false
+	for i, ch := range content {
+		if ch == '#' {
+			if inHashtag >= 0 {
+				inHashtag = -1
+				ignoreCurrentWord = true
+			} else {
+				inHashtag = i
+			}
+			continue
+		}
+
+		isWordBoundary := !unicode.IsLetter(ch) && !unicode.IsNumber(ch) && ch != '_' && ch != '-'
+		if isWordBoundary {
+			if inHashtag >= 0 {
+				hashtagStr := content[inHashtag:i]
+				hashtags = append(hashtags, hashtagStr)
+
+				inHashtag = -1
+				ignoreCurrentWord = false
+			}
+			continue
+		}
+	}
+
+	if inHashtag >= 0 && !ignoreCurrentWord {
+		hashtagStr := content[inHashtag:]
+		hashtags = append(hashtags, hashtagStr)
+	}
+
+	// remove duplicates
+	uniqueHashtags := []string{}
+	seen := map[string]bool{}
+	for _, hashtag := range hashtags {
+		if !seen[hashtag] {
+			uniqueHashtags = append(uniqueHashtags, hashtag)
+			seen[hashtag] = true
+		}
+	}
+
+	return uniqueHashtags
 }
