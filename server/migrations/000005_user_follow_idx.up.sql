3057775ab486a1ba0a2b91ebc8cfcb82cbd40502
tinaxd <31384595+tinaxd@users.noreply.github.com>
2024-01-10 17:30:29 +0900

    index fix

--- server/migrations/000005_user_follow_idx.up.sql
+++ server/migrations/000005_user_follow_idx.up.sql
@@ -1,3 +1,3 @@
-CREATE UNIQUE INDEX `user_follow_unique_idx` ON UserFollow(followerId, followeeId);
+CREATE UNIQUE INDEX `user_follow_unique_idx` ON UserFollow(follower_id, followee_id);
 
-CREATE INDEX `user_follow_follower_idx` ON UserFollow(followerId);
+CREATE INDEX `user_follow_follower_idx` ON UserFollow(follower_id);
