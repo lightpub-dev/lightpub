64a8fe39e4fbbb21b4c888433c9e3b60e58d40c5
tinaxd <31384595+tinaxd@users.noreply.github.com>
2024-01-25 20:29:55 +0900

    post serializer wip

--- dj/api/serializers/post.py
+++ dj/api/serializers/post.py
@@ -1,3 +1,37 @@
 from rest_framework import serializers
 
-from api.models import User, UserFollow, UserProfileLabel, UserToken
+from api.models import User, UserFollow, UserProfileLabel, UserToken, Post
+
+
+class PostAuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "username", "host", "nickname"]
+
+
+class PostEntrySerializer(serializers.ModelSerializer):
+    author = PostAuthorSerializer()
+    reposted_by_me = serializers.BooleanField(allow_null=True)
+    favorited_by_me = serializers.BooleanField(allow_null=True)
+    bookmarked_by_me = serializers.BooleanField(allow_null=True)
+    repost_count = serializers.IntegerField(allow_null=True)
+    favorite_count = serializers.IntegerField(allow_null=True)
+    reply_count = serializers.IntegerField(allow_null=True)
+    quote_count = serializers.IntegerField(allow_null=True)
+    reactions = serializers.DictField(child=serializers.IntegerField(), allow_null=True)
+
+    class Meta:
+        model = Post
+        fields = ["id", "content", "created_at", "privacy"]
+
+
+class CreatePostSerializer(serializers.ModelSerializer):
+    def save(self, validated_data):
+        """
+        Additional `poster_id` field is required to save a post.
+        Accept `reply_to_id` or `repost_of_id` or `
+        """
+
+    class Meta:
+        model = Post
+        fields = ["content", "privacy"]
