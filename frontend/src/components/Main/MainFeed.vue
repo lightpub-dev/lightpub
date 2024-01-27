8a4b435a1a84bc51320467d39898836f9c6c76a9
tinaxd <31384595+tinaxd@users.noreply.github.com>
2024-01-27 14:08:32 +0900

    Refactor infinite scrolling logic in MainFeed and TrendPostList components

--- frontend/src/components/Main/MainFeed.vue
+++ frontend/src/components/Main/MainFeed.vue
@@ -1,24 +1,8 @@
 <script lang="ts" setup>
-import { computed } from 'vue'
+import { computed, ref, watchEffect } from 'vue'
 import { useTimeline } from '../UserPost/processFeedPosts.ts'
-import InfiniteLoading from 'v3-infinite-loading'
-import 'v3-infinite-loading/lib/style.css'
 
 const timeline = useTimeline()
-const load = async $state => {
-    try {
-        await timeline.fetchNext()
-
-        if (timeline.hasNext.value === false) {
-            $state.complete()
-        } else {
-            $state.loaded()
-        }
-    } catch (e) {
-        console.error(e)
-        $state.error()
-    }
-}
 
 const feedPosts = computed(() => {
     if (timeline.posts.value === null) {
@@ -30,11 +14,46 @@ const feedPosts = computed(() => {
 eventBus.on('post-created', async () => {
     await timeline.fetchPosts()
 })
+
+const divRef = ref<HTMLDivElement | null>(null)
+
+let loadingNow = false
+const scrolledToBottom = async (e: Event) => {
+    if (loadingNow) {
+        return
+    }
+    try {
+        loadingNow = true
+        await timeline.fetchNext()
+    } catch (ex) {
+        console.error(ex)
+        alert('Failed to fetch next posts')
+    } finally {
+        loadingNow = false
+    }
+}
+
+watchEffect(() => {
+    if (divRef.value) {
+        const myDiv = divRef.value
+        divRef.value.addEventListener('scroll', e => {
+            // console.log('left hand', myDiv.offsetHeight + myDiv.scrollTop)
+            // console.log('right hand', myDiv.scrollHeight)
+            if (
+                myDiv.offsetHeight + myDiv.scrollTop >=
+                myDiv.scrollHeight * 0.8
+            ) {
+                scrolledToBottom(e)
+            }
+        })
+    }
+})
 </script>
 
 <template>
     <div
-        class="grid-cols-1 w-full grid md:grid-cols-1 px-20 pt-5 transition-all bg-gray-100"
+        class="grid-cols-1 w-full grid md:grid-cols-1 px-20 pt-5 transition-all bg-gray-100 overflow-y-auto"
+        ref="divRef"
     >
         <div class="flex flex-col p-2">
             <UserPost
@@ -42,7 +61,6 @@ eventBus.on('post-created', async () => {
                 :key="index"
                 :user_post="post"
             ></UserPost>
-            <InfiniteLoading @infinite="load" />
         </div>
     </div>
 </template>
--- frontend/src/components/Trend/TrendPostList.vue
+++ frontend/src/components/Trend/TrendPostList.vue
@@ -10,20 +10,67 @@ const hashtag = computed(() => route.query.hashtag as string)
 const axios = inject(AUTH_AXIOS)!
 
 const posts: Ref<UserPostEntry[]> = ref([])
-watchEffect(async () => {
-    posts.value = []
-    const { data } = await axios.get<{ results: UserPostEntry[] }>(`/posts`, {
-        params: {
-            hashtag: hashtag.value
-        }
+const nextURL = ref<string | null>('')
+const fetchNext = async () => {
+    if (nextURL.value === null) {
+        return
+    }
+    const params = {
+        hashtag: hashtag.value
+    } as any
+    if (nextURL.value !== '' && nextURL.value !== null) {
+        params.next = nextURL.value
+    }
+    const { data } = await axios.get<{
+        results: UserPostEntry[]
+        next: string | null
+    }>(`/posts`, {
+        params
     })
     posts.value = data.results
+    nextURL.value = data.next
+}
+watchEffect(async () => {
+    await fetchNext()
+})
+
+const divRef = ref<HTMLDivElement | null>(null)
+let loadingNow = false
+const scrolledToBottom = async (e: Event) => {
+    if (loadingNow) {
+        return
+    }
+    try {
+        loadingNow = true
+        await fetchNext()
+    } catch (ex) {
+        console.error(ex)
+        alert('Failed to fetch next posts')
+    } finally {
+        loadingNow = false
+    }
+}
+watchEffect(() => {
+    if (divRef.value) {
+        const myDiv = divRef.value
+        divRef.value.addEventListener('scroll', e => {
+            // console.log('left hand', myDiv.offsetHeight + myDiv.scrollTop)
+            // console.log('right hand', myDiv.scrollHeight)
+            if (
+                myDiv.offsetHeight + myDiv.scrollTop >=
+                myDiv.scrollHeight * 0.8
+            ) {
+                scrolledToBottom(e)
+            }
+        })
+    }
 })
 </script>
 
 <template>
     <div
-        class="grid-cols-1 w-full grid md:grid-cols-1 px-20 pt-5 transition-all bg-gray-100"
+        class="grid-cols-1 w-full grid md:grid-cols-1 px-20 pt-5 transition-all bg-gray-100 overflow-y-auto"
+        ref="divRef"
     >
         <div class="text-left text-lg">
             <span class="text-gray-500">#</span>
