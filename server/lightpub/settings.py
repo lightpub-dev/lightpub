f40fad7372fbf888a6b52195b2d3346932c66800
tinaxd <31384595+tinaxd@users.noreply.github.com>
2024-02-04 15:01:01 +0900

    webfinger

--- server/lightpub/settings.py
+++ server/lightpub/settings.py
@@ -15,6 +15,9 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Hostname
+HOSTNAME = "localhost:8000"
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
