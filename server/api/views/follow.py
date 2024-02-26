7430424fab226cabb3f6ffe52c7e413c7da3923e
tinaxd <31384595+tinaxd@users.noreply.github.com>
2024-02-27 02:14:32 +0900

    Refactor follow.py views

--- server/api/views/follow.py
+++ server/api/views/follow.py
@@ -1,14 +1,8 @@
-from rest_framework import generics, mixins
+from rest_framework import generics
 from rest_framework.response import Response
-from rest_framework.reverse import reverse
 
 from api.auth.permission import AuthOnlyPermission
 from api.serializers.follow import CreateFollowSerializer
-from api.utils.users import UserSpecifier
-
-from ..jsonld.mixins import JsonldAwareMixin
-from ..models import UserFollow
-from ..serializers.user import UserFollowSerializer
 
 
 class CreateFollowView(generics.CreateAPIView):
@@ -22,70 +16,3 @@ class CreateFollowView(generics.CreateAPIView):
         ser.save()
 
         return Response(status=201)
-
-
-class UserFollowerView(
-    JsonldAwareMixin, mixins.ListModelMixin, generics.GenericAPIView
-):
-    serializer_class = UserFollowSerializer
-    permission_classes = []
-
-    def list(self, request, *args, **kwargs):
-        if not self.jsonld_requested():
-            return super().list(request, *args, **kwargs)
-
-        queryset = self.filter_queryset(self.get_queryset())
-        page = self.paginate_queryset(queryset)
-        if page is not None:
-            followers = [follower.follower for follower in page]
-            urls = [
-                reverse("api:user-detail", kwargs={"pk": follower.id}, request=request)
-                for follower in followers
-            ]
-            return self.get_paginated_response(urls)
-
-        raise ValueError("page is None")
-
-    def get_queryset(self):
-        target_user = UserSpecifier.parse_str(self.kwargs["user_spec"]).get_user_model()
-        return UserFollow.objects.filter(followee=target_user).order_by("-created_at")
-
-    def get_serializer_context(self):
-        return {
-            "request": self.request,
-        }
-
-
-class UserFollowingView(
-    JsonldAwareMixin,
-    mixins.ListModelMixin,
-    generics.GenericAPIView,
-):
-    serializer_class = UserFollowSerializer
-    permission_classes = []
-
-    def list(self, request, *args, **kwargs):
-        if not self.jsonld_requested():
-            return super().list(request, *args, **kwargs)
-
-        queryset = self.filter_queryset(self.get_queryset())
-
-        page = self.paginate_queryset(queryset)
-        if page is not None:
-            followees = [followee.followee for followee in page]
-            urls = [
-                reverse("api:user-detail", kwargs={"pk": followee.id}, request=request)
-                for followee in followees
-            ]
-            return self.get_paginated_response(urls)
-
-        raise ValueError("page is None")
-
-    def get_queryset(self):
-        target_user = UserSpecifier.parse_str(self.kwargs["user_spec"]).get_user_model()
-        return UserFollow.objects.filter(follower=target_user).order_by("-created_at")
-
-    def get_serializer_context(self):
-        return {
-            "request": self.request,
-        }
