d4bf0fea51a5fb97d1c0a9db6d66d816b7cd2edc
tinaxd <31384595+tinaxd@users.noreply.github.com>
2024-02-28 00:51:54 +0900

    moved find_hashtags to utils

--- server/api/serializers/post.py
+++ server/api/serializers/post.py
@@ -15,7 +15,7 @@ from api.models import (
     UploadedFile,
     User,
 )
-from api.requester import get_requester
+from api.utils.post import find_hashtags
 
 
 class PostAuthorSerializer(serializers.ModelSerializer):
@@ -67,37 +67,6 @@ class RepostOfIdField(serializers.PrimaryKeyRelatedField):
         # posts that are visible to the user
         posts = Post.objects.filter(privacy__in=[0, 1])  # public  # unlisted
         return posts
-
-
-def _find_hashtags(content: str) -> list[str]:
-    hashtags = []
-    in_hashtag = False
-    hashtag_start_index = 0
-    for i, ch in enumerate(content):
-        if ch == "#":
-            if not in_hashtag:
-                in_hashtag = True
-                hashtag_start_index = i
-            else:
-                # Reset if another # is found immediately after
-                in_hashtag = False
-        elif not ch.isalnum() and ch not in ["_", "-"]:
-            # Word boundary detected
-            if in_hashtag:
-                hashtag = content[hashtag_start_index:i]
-                if hashtag != "#":  # Ignore single '#' entries
-                    hashtags.append(hashtag[1:])
-                in_hashtag = False
-        # Note: The Go code snippet handles end-of-content logic implicitly
-    # Check if the content ends with a hashtag
-    if in_hashtag:
-        hashtag = content[hashtag_start_index:]
-        if hashtag != "#":
-            hashtags.append(hashtag[1:])
-
-    # Remove duplicates
-    unique_hashtags = list(set(hashtags))
-    return unique_hashtags
 
 
 class UploadedFileSerializer(serializers.ModelSerializer):
@@ -281,7 +250,7 @@ class PostSerializer(serializers.ModelSerializer):
 
             # find hashtags
             if post.content is not None:
-                hashtags = _find_hashtags(post.content)
+                hashtags = find_hashtags(post.content)
             else:
                 # fetch hashtags of the original post
                 repost_of = Post.objects.get(id=post.repost_of_id)
