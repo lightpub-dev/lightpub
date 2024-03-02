b9dcf6490d403e4e0c3a8cfe8be6ec51accf0a28
tinaxd <31384595+tinaxd@users.noreply.github.com>
2024-03-02 13:09:14 +0900

    naming convention check

--- server/api/handler.go
+++ server/api/handler.go
@@ -74,7 +74,7 @@ func BuildEcho(h *Handler, options EchoOptions) *echo.Echo {
 	// Users
 	unAuthed.GET("/user/:username/posts", h.GetUserPosts)
 	unAuthed.GET("/user/:username/followers", h.GetUserFollowers)
-	unAuthed.GET("/user/:username/followings", h.GetUserFollowing)
+	unAuthed.GET("/user/:username/following", h.GetUserFollowing)
 	authed.PUT("/user/:username/follow", h.FollowAUser)
 	authed.DELETE("/user/:username/follow", h.UnfollowAUser)
 	authed.PATCH("/user/:userspec", h.PutUser)
