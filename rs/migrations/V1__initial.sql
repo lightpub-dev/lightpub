fbca8ee98192e9d647be8bc66b51d03556a4bb88
tinaxd <31384595+tinaxd@users.noreply.github.com>
2024-03-04 22:14:53 +0900

    varchar to char

--- rs/migrations/V1__initial.sql
+++ rs/migrations/V1__initial.sql
@@ -1,10 +1,10 @@
 CREATE TABLE `users` (
-    `id` varchar(32) NOT NULL,
+    `id` CHAR(32) NOT NULL,
     `username` varchar(64) NOT NULL,
     `host` varchar(128) DEFAULT NULL,
     `bpasswd` varchar(60) NOT NULL,
     `nickname` varchar(255) NOT NULL,
-    `avatar_id` VARCHAR(32) NOT NULL,
+    `avatar_id` CHAR(32) NOT NULL,
     `bio` text NOT NULL,
     `url` varchar(512) DEFAULT NULL,
     `inbox` varchar(512) DEFAULT NULL,
@@ -17,15 +17,15 @@ CREATE TABLE `users` (
 );
 
 CREATE TABLE `posts` (
-    `id` varchar(32) NOT NULL,
-    `poster_id` varchar(32) DEFAULT NULL,
+    `id` CHAR(32) NOT NULL,
+    `poster_id` CHAR(32) DEFAULT NULL,
     `content` longtext DEFAULT NULL,
     `inserted_at` datetime(6) NOT NULL,
     `created_at` datetime(6) NOT NULL,
     `deleted_at` datetime(6) DEFAULT NULL,
     `privacy` enum('public', 'unlisted', 'follower', 'private') NOT NULL,
-    `reply_to_id` varchar(32) DEFAULT NULL,
-    `repost_of_id` varchar(32) DEFAULT NULL,
+    `reply_to_id` CHAR(32) DEFAULT NULL,
+    `repost_of_id` CHAR(32) DEFAULT NULL,
     PRIMARY KEY (`id`),
     KEY `fk_posts_poster` (`poster_id`),
     KEY `fk_posts_reply_to` (`reply_to_id`),
@@ -36,10 +36,10 @@ CREATE TABLE `posts` (
 );
 
 CREATE TABLE `uploaded_files` (
-    `id` varchar(32) NOT NULL,
+    `id` CHAR(32) NOT NULL,
     `file_ext` varchar(128) NOT NULL,
     `created_at` datetime(6) NOT NULL,
-    `uploaded_by_id` varchar(32) NOT NULL,
+    `uploaded_by_id` CHAR(32) NOT NULL,
     PRIMARY KEY (`id`),
     KEY `fk_uploaded_files_uploaded_by` (`uploaded_by_id`),
     CONSTRAINT `fk_uploaded_files_uploaded_by` FOREIGN KEY (`uploaded_by_id`) REFERENCES `users` (`id`)
@@ -52,8 +52,8 @@ ADD
 
 CREATE TABLE `user_follows` (
     `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-    `follower_id` varchar(32) NOT NULL,
-    `followee_id` varchar(32) NOT NULL,
+    `follower_id` CHAR(32) NOT NULL,
+    `followee_id` CHAR(32) NOT NULL,
     `created_at` datetime(6) DEFAULT NULL,
     PRIMARY KEY (`id`),
     UNIQUE KEY `idx_follower_followee` (`follower_id`, `followee_id`),
@@ -63,7 +63,7 @@ CREATE TABLE `user_follows` (
 );
 
 CREATE TABLE `user_labels` (
-    `user_id` varchar(32) NOT NULL,
+    `user_id` CHAR(32) NOT NULL,
     `order` bigint(20) NOT NULL,
     `key` text NOT NULL,
     `value` text NOT NULL,
@@ -73,7 +73,7 @@ CREATE TABLE `user_labels` (
 
 CREATE TABLE `user_tokens` (
     `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-    `user_id` varchar(32) DEFAULT NULL,
+    `user_id` CHAR(32) DEFAULT NULL,
     `token` varchar(64) NOT NULL,
     `created_at` datetime(6) NOT NULL,
     `last_used_at` datetime(6) NOT NULL,
@@ -83,9 +83,9 @@ CREATE TABLE `user_tokens` (
 );
 
 CREATE TABLE `post_attachments` (
-    `id` varchar(32) NOT NULL,
-    `post_id` varchar(32) DEFAULT NULL,
-    `uploaded_file_id` varchar(32) NOT NULL,
+    `id` CHAR(32) NOT NULL,
+    `post_id` CHAR(32) DEFAULT NULL,
+    `uploaded_file_id` CHAR(32) NOT NULL,
     PRIMARY KEY (`id`),
     KEY `fk_post_attachments_uploaded_file` (`uploaded_file_id`),
     KEY `fk_post_attachments_post` (`post_id`),
@@ -95,8 +95,8 @@ CREATE TABLE `post_attachments` (
 
 CREATE TABLE `post_favorites` (
     `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-    `post_id` varchar(32) NOT NULL,
-    `user_id` varchar(32) NOT NULL,
+    `post_id` CHAR(32) NOT NULL,
+    `user_id` CHAR(32) NOT NULL,
     `is_bookmark` tinyint(1) NOT NULL,
     `created_at` datetime(6) DEFAULT NULL,
     PRIMARY KEY (`id`),
@@ -108,7 +108,7 @@ CREATE TABLE `post_favorites` (
 
 CREATE TABLE `post_hashtags` (
     `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-    `post_id` varchar(32) NOT NULL,
+    `post_id` CHAR(32) NOT NULL,
     `hashtag_name` longtext NOT NULL,
     PRIMARY KEY (`id`),
     KEY `fk_posts_hashtags` (`post_id`),
@@ -117,8 +117,8 @@ CREATE TABLE `post_hashtags` (
 
 CREATE TABLE `post_mentions` (
     `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-    `post_id` varchar(32) NOT NULL,
-    `target_user_id` varchar(32) NOT NULL,
+    `post_id` CHAR(32) NOT NULL,
+    `target_user_id` CHAR(32) NOT NULL,
     PRIMARY KEY (`id`),
     KEY `fk_post_mentions_target_user` (`target_user_id`),
     KEY `fk_posts_mentions` (`post_id`),
@@ -133,9 +133,9 @@ create table `reactions` (
 
 CREATE TABLE `post_reactions` (
     `id` bigint(20) unsigned NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    `post_id` varchar(32) NOT NULL,
+    `post_id` CHAR(32) NOT NULL,
     `reaction_id` bigint unsigned NOT NULL,
-    `user_id` varchar(32) NOT NULL,
+    `user_id` CHAR(32) NOT NULL,
     `created_at` datetime(6) DEFAULT NULL,
     KEY `fk_post_reactions_post` (`post_id`),
     KEY `fk_post_reactions_user` (`user_id`),
@@ -147,7 +147,7 @@ CREATE TABLE `post_reactions` (
 ALTER TABLE
     `users`
 MODIFY
-    COLUMN `avatar_id` VARCHAR(32) NULL DEFAULT NULL;
+    COLUMN `avatar_id` CHAR(32) NULL DEFAULT NULL;
 
 ALTER TABLE
     lightpub.post_hashtags DROP FOREIGN KEY fk_posts_hashtags;
@@ -173,11 +173,11 @@ AFTER
     `url`;
 
 CREATE TABLE user_follow_requests(
-    id VARCHAR(32) NOT NULL PRIMARY KEY,
+    id CHAR(32) NOT NULL PRIMARY KEY,
     uri VARCHAR(512) NOT NULL UNIQUE,
     incoming TINYINT(1) NOT NULL,
-    follower_id VARCHAR(32) NOT NULL,
-    followee_id VARCHAR(32) NOT NULL,
+    follower_id CHAR(32) NOT NULL,
+    followee_id CHAR(32) NOT NULL,
     created_at DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
     CONSTRAINT fk_users_follow_request_follower FOREIGN KEY (follower_id) REFERENCES users(id),
     CONSTRAINT fk_users_follow_request_followee FOREIGN KEY (followee_id) REFERENCES users(id)
@@ -185,14 +185,14 @@ CREATE TABLE user_follow_requests(
 
 CREATE TABLE `user_keys` (
     id VARCHAR(512) NOT NULL PRIMARY KEY,
-    owner_id VARCHAR(32) NOT NULL,
+    owner_id CHAR(32) NOT NULL,
     public_key TEXT NOT NULL,
     updated_at DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
     CONSTRAINT fk_users_key_owner FOREIGN KEY (owner_id) REFERENCES users(id)
 );
 
 CREATE TABLE remote_users(
-    user_id VARCHAR(32) NOT NULL PRIMARY KEY,
+    user_id CHAR(32) NOT NULL PRIMARY KEY,
     `following` VARCHAR(512) NULL DEFAULT NULL,
     followers VARCHAR(512) NULL DEFAULT NULL,
     liked VARCHAR(512) NULL DEFAULT NULL,
