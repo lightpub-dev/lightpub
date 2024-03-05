f2af22a3f6201c921c57fc6177897795da55d60a
tinaxd <31384595+tinaxd@users.noreply.github.com>
2024-03-05 12:31:22 +0900

    Add Post module and related structs

---
+++ rs/src/services/post.rs
@@ -0,0 +1,25 @@
+#[derive(Debug)]
+pub struct DBUserService {
+    pool: MySqlPool,
+}
+
+impl DBUserService {
+    pub fn new(pool: MySqlPool) -> Self {
+        Self { pool }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum PostPrivacy {
+    Public,
+    Unlisted,
+    Followers,
+    Private,
+}
+
+#[derive(Debug, Clone)]
+pub struct PostCreateRequest {
+    poster_id: Simple,
+    content: String,
+    privacy: PostPrivacy,
+}
