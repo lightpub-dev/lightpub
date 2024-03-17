7bcc6c9a46a56c845d3f01c9fb239e16ccfde8ee
tinaxd <31384595+tinaxd@users.noreply.github.com>
2024-03-17 18:08:14 +0900

    test framework

--- rs/tests/register.test.ts
+++ rs/tests/register.test.ts
@@ -1,33 +1,65 @@
 import axios from "axios";
 import { test, describe, expect, beforeAll } from "bun:test";
 
+const BASE_URL = "https://lightpub.tinax.local";
+
 describe("/register", () => {
-    test("registering a new user", async () => {
-        const response = await axios.post("http://localhost:8000/register", {
-            username: "initialuser",
-            nickname: "initialnick",
-            password: "password",
-        });
-        expect(response.status).toBe(200);
-        expect(response.data).toHaveProperty("user_id");
-    });
-    test("registering duplicate users", async () => {
-        {
-            const response = await axios.post("http://localhost:8000/register", {
-                username: "duplicateduser",
-                nickname: "duplicatednick",
-                password: "password",
-            });
+    test(
+        "registering a new user",
+        async () => {
+            const response = await axios.post(
+                BASE_URL + "/register",
+                {
+                    username: "initialuser",
+                    nickname: "initialnick",
+                    password: "password",
+                },
+                {
+                    timeout: 30000,
+                }
+            );
             expect(response.status).toBe(200);
             expect(response.data).toHaveProperty("user_id");
-        }
+        },
         {
-            const response = await axios.post("http://localhost:8000/register", {
-                username: "duplicateduser",
-                nickname: "duplicatednick",
-                password: "password",
-            });
-            expect(response.status).toBe(400);
+            timeout: 30000,
         }
-    });
+    );
+    test(
+        "registering duplicate users",
+        async () => {
+            {
+                const response = await axios.post(
+                    BASE_URL + "/register",
+                    {
+                        username: "duplicateduser",
+                        nickname: "duplicatednick",
+                        password: "password",
+                    },
+                    {
+                        timeout: 30000,
+                    }
+                );
+                expect(response.status).toBe(200);
+                expect(response.data).toHaveProperty("user_id");
+            }
+            {
+                const response = await axios.post(
+                    BASE_URL + "/register",
+                    {
+                        username: "duplicateduser",
+                        nickname: "duplicatednick",
+                        password: "password",
+                    },
+                    {
+                        timeout: 30000,
+                    }
+                );
+                expect(response.status).toBe(400);
+            }
+        },
+        {
+            timeout: 60000,
+        }
+    );
 });
