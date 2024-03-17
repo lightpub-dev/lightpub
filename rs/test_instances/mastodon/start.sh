b8d992fa1bff1851a2e27347ceb6e9020b77c0a9
tinaxd <31384595+tinaxd@users.noreply.github.com>
2024-03-17 12:59:30 +0900

    auto user create

--- rs/test_instances/mastodon/start.sh
+++ rs/test_instances/mastodon/start.sh
@@ -1,2 +1,7 @@
-#!/bin/sh
-bundle exec rake db:migrate && bundle exec rails s -p 3000
+#!/bin/bash
+create_account() {
+  echo "Creating account"
+  tootctl accounts create massuser --email massuser@mastodon.tinax.local --confirmed
+}
+
+bundle exec rake db:migrate && create_account; bundle exec rails s -p 3000
