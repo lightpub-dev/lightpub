6887a83548cf3e1fa90532034c51ab6e1df0ce38
tinaxd <31384595+tinaxd@users.noreply.github.com>
2024-03-18 16:13:55 +0900

    remove python server

--- server/api/utils/posts/privacy.py
+++
@@ -1,8 +0,0 @@
-from enum import IntEnum
-
-
-class PostPrivacy(IntEnum):
-    PUBLIC = 0
-    UNLISTED = 1
-    FOLLOWERS = 2
-    PRIVATE = 3
--- server/gunicorn.conf.py
+++
@@ -1,5 +0,0 @@
-wsgi_app = "lightpub.wsgi:application"
-workers = 4
-max_requests = 1000
-max_requests_jitter = 100
-timeout = 30
