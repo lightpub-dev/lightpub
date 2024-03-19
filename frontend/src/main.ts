c1e0aa5eb66e137991524519fe451aa9b179fcea
tinaxd <31384595+tinaxd@users.noreply.github.com>
2024-03-19 15:44:24 +0900

    vue proxy

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -7,8 +7,6 @@ import App from './App.vue'
 import router from './router'
 import axios from 'axios'
 
-axios.defaults.baseURL = 'https://lightpub.tinax.local/api'
-
 const app = createApp(App)
 
 app.use(createPinia())
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,12 +5,16 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  base: '/web',
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server: {
+    proxy: {
+      '^/(?!web).*': 'http://localhost:8000'
     }
   }
 })
