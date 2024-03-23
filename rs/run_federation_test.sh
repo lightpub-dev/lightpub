6f0e443517afb45521d042745f4b8d5281977c3d
tinaxd <31384595+tinaxd@users.noreply.github.com>
2024-03-23 12:46:09 +0900

    runner

--- rs/run_federation_test.sh
+++ rs/run_federation_test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -x
 echo "=== TEST ==="
 docker compose -f docker-compose.fed.yml --profile misskey --profile lightpub --profile testing down -v
-docker compose -f docker-compose.fed.yml --profile misskey --profile lightpub --profile testing up --build --exit-code-from test-runner
+docker compose -f docker-compose.fed.yml --profile misskey --profile lightpub --profile testing up --build --exit-code-from test_runner
