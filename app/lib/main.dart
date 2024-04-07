9c3a6c99aecb7bda3d972b12fbfb888abf97c91f
tinaxd <31384595+tinaxd@users.noreply.github.com>
2024-04-07 20:55:57 +0900

    Add LoginView widget to app/lib/login.dart

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -104,15 +104,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // action in the IDE, or press "p" in the console), to see the
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+          children: <Widget>[],
         ),
       ),
       floatingActionButton: FloatingActionButton(
