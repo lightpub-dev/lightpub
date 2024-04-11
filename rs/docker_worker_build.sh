1312caccf9b1416a040afa7915db78f951669bf7
tinaxd <31384595+tinaxd@users.noreply.github.com>
2024-04-11 17:09:29 +0900

    tests

--- rs/docker_worker_build.sh
+++ rs/docker_worker_build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-if [ $1 = "debug"]; then
-    cargo build --package worker && cp target/debug/lightpub_api /out/lightpub_api
-elif [ $1 = "release" ]; then
-    cargo build --release --package worker && cp target/release/lightpub_api /out/lightpub_api
+if [ "$1" = "debug" ]; then
+    cargo build --package lightpub_worker && cp target/debug/lightpub_worker /out/lightpub_worker
+elif [ "$1" = "release" ]; then
+    cargo build --release --package lightpub_worker && cp target/release/lightpub_worker /out/lightpub_worker
 else
     echo "Usage: $0 [debug|release]" && exit 1
 fi
