a1fa0c2e59544a2d6d7974fc7300b4b0acd654b1
tinaxd <31384595+tinaxd@users.noreply.github.com>
2024-09-05 17:20:44 +0900

    PostContent

--- server/src/domain/model/post.ts
+++ server/src/domain/model/post.ts
@@ -1,4 +1,5 @@
 import { Clock } from "../../utils/clock";
+import { ValueObject } from "../../utils/eq";
 import { ObjectID } from "./object_id";
 
 export class Post {
@@ -6,7 +7,7 @@ export class Post {
     public id: ObjectID,
     public url: string,
     public authorId: string,
-    public content: string | null,
+    public content: PostContent | null,
     public privacy: "public" | "unlisted" | "follower" | "private",
     public replyToId: string | null,
     public repostOfId: string | null,
@@ -19,4 +20,19 @@ export class Post {
     public createdAt: Clock,
     public deletedAt: Clock | null
   ) {}
+}
+
+export class PostContent implements ValueObject {
+  constructor(private content: string) {
+    if (content.length > 2048) {
+      throw new Error("Content too long");
+    }
+  }
+
+  equals(other: ValueObject): boolean {
+    if (other instanceof PostContent) {
+      return this.content === other.content;
+    }
+    return false;
+  }
 }
