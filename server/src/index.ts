e180f9e04771720c3d0d5ed6effe18f11c2bc88e
tinaxd <31384595+tinaxd@users.noreply.github.com>
2024-09-05 15:29:57 +0900

    hono

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,10 +5,6 @@ import { Hono } from "hono";
 import { db } from "./db";
 import { sql } from "drizzle-orm";
 
-const query = sql`select "hello world" as text`;
-const result = db.get<{ text: string }>(query);
-console.log(result);
-
 const app = new Hono();
 
 app.get("/", (c) => {
