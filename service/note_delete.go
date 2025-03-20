9fbbbeff25f1e6c94a860005ebf9f7f856195d9e
tinaxd <31384595+tinaxd@users.noreply.github.com>
2025-03-20 23:42:48 +0900

    note dleete

---
+++ service/note_delete.go
@@ -0,0 +1,71 @@
+package service
+
+import (
+	"context"
+	"errors"
+	"net/http"
+	"time"
+
+	"github.com/lightpub-dev/lightpub/db"
+	"github.com/lightpub-dev/lightpub/types"
+	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
+)
+
+var (
+	ErrNoteDeletePermission = NewServiceError(http.StatusBadRequest, "note not found or not note author")
+)
+
+func (s *State) DeleteNoteByID(ctx context.Context, userID types.UserID, noteID types.NoteID) error {
+	err := s.WithTransaction(func(tx *State) error {
+		var note db.Note
+		if err := tx.DB(ctx).Where("id = ? AND author_id = ? AND deleted_at IS NULL", noteID, userID).Clauses(
+			clause.Locking{Strength: "UPDATE"},
+		).First(&note).Error; err != nil {
+			if errors.Is(err, gorm.ErrRecordNotFound) {
+				return ErrNoteDeletePermission
+			}
+			return err
+		}
+
+		if err := tx.DB(ctx).Where("id = ?", note.ID).Update("deleted_at", time.Now()).Error; err != nil {
+			return err
+		}
+
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+
+	// TODO: apub Delete
+
+	return nil
+}
+
+func (s *State) DeleteRenote(ctx context.Context, userID types.UserID, renoteTargetID types.NoteID) error {
+	err := s.WithTransaction(func(tx *State) error {
+		var renote db.Note
+		if err := tx.DB(ctx).Where("renote_of_id = ? AND author_id = ? AND content IS NULL", renoteTargetID, userID).Clauses(
+			clause.Locking{Strength: "UPDATE"},
+		).First(&renote).Error; err != nil {
+			if errors.Is(err, gorm.ErrRecordNotFound) {
+				return ErrNoteDeletePermission
+			}
+			return err
+		}
+
+		if err := tx.DB(ctx).Delete(&renote).Error; err != nil {
+			return err
+		}
+
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+
+	// TODO: apub Undo(Announce)
+
+	return nil
+}
